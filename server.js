8faccb684511bd1d04245daa7c10ffe570e5cf5b
Mulle21 <mulukenab95@gmail.com>
2024-06-07 07:18:32 -0700

    server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import express from 'express';
 import controllerRouting from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 27017;
+
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
-controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+app.use('/', indexRouter);
 
-export default app;
+module.exports = app;
