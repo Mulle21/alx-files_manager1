a62be8301e93d3efb1613c3073b81f9ee42d4e96
Mulle21 <mulukenab95@gmail.com>
2024-06-07 06:49:44 -0700

    taskport

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import controllerRouting from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 27017;
 
 app.use(express.json());
 
